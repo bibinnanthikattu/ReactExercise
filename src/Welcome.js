46bcd002d0d6787f88ffb3612f2540bd0be3c82e
Bibin Nandikattu Jose <bibinnanthikattu@hotmail.com>
2022-06-16 19:38:43 +0200

    Feat:Conditional Rendering -02

--- src/Welcome.js
+++ src/Welcome.js
@@ -12,7 +12,8 @@ export default class Welcome extends Component {
             {/* <p>Welcome {this.props.name = "Doe"} !</p> */}
             {/* you can't add a value to the name prop. but you can add a default value to the props. It will take the default value if no props is passed from the parent  */}
             {/* <p>Your age is {this.props.age} .</p> */}
-            {ageFilter > 18 ? <Age age={ageFilter} />: null } 
+            {/* {ageFilter > 18 ? <Age age={ageFilter} />: null }  */}
+            {ageFilter ? <Age age={ageFilter} /> :null}
       </div>
     )
   }
