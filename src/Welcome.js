cfa4c5102efebdfcd5af49d5dd596838b0272d73
Bibin Nandikattu Jose <bibinnanthikattu@hotmail.com>
2022-06-16 18:51:06 +0200

    Feat:Props-02

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
 export default class Welcome extends Component {
+    constructor(props) {
+        super(props)
+        
+    }
   render() {
     return (
         <div>
-            <p>Welcome {this.props.name} !</p>
+            <p>Welcome {this.props.name = "Doe"} !</p>
+            {/* you can't add a value to the name prop. but you can add a default value to the props. It will take the default value if no props is passed from the parent  */}
       </div>
     )
   }
