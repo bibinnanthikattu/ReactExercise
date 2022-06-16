fa71315d8c693b4444dde3008ccd005786845595
Bibin Nandikattu Jose <bibinnanthikattu@hotmail.com>
2022-06-16 19:34:08 +0200

    Feat:Conditional Rendering - 01

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react'
 import Age from './Age'
 
 export default class Welcome extends Component {
-    constructor(props) {
-        super(props)
-        
-    }
+  constructor(props) {
+    super(props)
+  }
   render() {
+        let ageFilter = this.props.age;
     return (
         <div>
             {/* <p>Welcome {this.props.name = "Doe"} !</p> */}
             {/* you can't add a value to the name prop. but you can add a default value to the props. It will take the default value if no props is passed from the parent  */}
             {/* <p>Your age is {this.props.age} .</p> */}
-            <Age age={this.props.age} />
+            {ageFilter > 18 ? <Age age={ageFilter} />: null } 
       </div>
     )
   }
