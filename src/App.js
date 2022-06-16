33351c3b198c4ef508ee332aad1e03791d40ea70
Bibin Nandikattu Jose <bibinnanthikattu@hotmail.com>
2022-06-16 17:59:09 +0200

    Feat:JSX-05

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 
 function App() {
   // const hello = "Hello, World !"
-  const printName = (name) => <h1>Hello, {name}</h1>
+  // const printName = (name) => <h1>Hello, {name}</h1>
+  let getSum = (a, b) => <h2>{a + b}</h2>
   return (
     <>
-    {printName("Jhon")}
+      {/* {printName("Jhon")} */}
+      {getSum(10,20)}
     </>
   );
 }
