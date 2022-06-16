df4f9298ba6f946bcacdc64c8af9157edcdf772c
Bibin Nandikattu Jose <bibinnanthikattu@hotmail.com>
2022-06-16 18:37:25 +0200

    Feat:components -06

--- src/Hello.js
+++ src/Hello.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react'
 
 export default class Hello extends Component {
   render() {
-    return (
-      <h1>Hello, world!</h1>
+      return (
+        <>
+              <h1>Hello, world!</h1>
+              <p>What a beautiful day!</p>
+        </>
     )
   }
 }
