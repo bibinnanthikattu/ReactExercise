a7c80f8546c37b039833dbb3b7aede310af4910a
Bibin Nandikattu Jose <bibinnanthikattu@hotmail.com>
2022-06-21 00:17:05 +0200

    Feat:Forms - 04

--- src/Login.jsx
+++ src/Login.jsx
@@ -13,13 +13,18 @@ export default class Login extends Component {
         const checked = e.target.checked;
         this.setState({[name]:type==='checkbox'? checked:value})
     }
+    handleResetBtn = () => {
+        this.setState({ username: '', password: '', remember: false })
+        // console.log(this.state);
+    }
   render() {
     return (
         <div>
             <input type="text" name='username' value={this.state.username} onChange={this.handleInput} />
             <input type="password" name='password' value={this.state.password} onChange={this.handleInput}/>
             <input type="checkbox" name='remember' checked={this.state.remember} onChange={this.handleInput}/>
-            <button disabled={!this.state.username && !this.state.password} onClick={()=>this.props.func(this.state)}>Login</button>
+            <button disabled={!this.state.username && !this.state.password} onClick={() => this.props.func(this.state)}>Login</button>
+            <button onClick={this.handleResetBtn}>Reset</button>
       </div>
     )
   }
