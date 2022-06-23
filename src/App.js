3b74f35a1aadf2f6e4f7f1f13386bc138a4dfda9
Bibin Nandikattu Jose <bibinnanthikattu@hotmail.com>
2022-06-23 16:56:37 +0200

    Feat:Component Composition - 01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from './Container';
+import Children from './Children';
 
 function App() {
   return (
-<></>
+    <>
+      <Container>
+        {<h1>This is an example for Component composition </h1>}
+        <Children />
+      </Container>
+    </>
   );
 }
 
---
+++ src/Container.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Container extends Component {
+    render() {
+        const styles = {
+            backgroundColor: "white",
+            border: '1px solid red',
+      }
+    return (
+        <div style={styles}>
+            {this.props.children}
+      </div>
+    )
+  }
+}
