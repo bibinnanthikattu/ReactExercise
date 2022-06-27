9439b198768cceea038bfdda335b365998e91191
Bibin Nandikattu Jose <bibinnanthikattu@hotmail.com>
2022-06-27 18:09:26 +0200

    Feat:Context - 02

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -3,14 +3,24 @@ import { LanguageContext } from './LanguageContext'
 
 export default class DisplayLanguage extends Component {
     render() {
-
+        const languages = {
+            en: {
+        CURRENT_LANGUAGE: "ENGLISH"
+            },
+            it: {
+        CURRENT_LANGUAGE: "ITALIAN"
+            },
+            mal: {
+                CURRENT_LANGUAGE: "MALAYALAM"
+            }
+}
 
     return (
         <div>
             <LanguageContext.Consumer>
                 {(language) => {
                     return (
-                        <h3>This is the cosumer</h3>
+                        <h1>Current Language is : {languages[language].CURRENT_LANGUAGE}</h1>
                     )
                 }}
             </LanguageContext.Consumer>
