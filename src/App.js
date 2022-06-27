7161cb3554c4c29ab92ea4e992e3f72f33b2f711
Bibin Nandikattu Jose <bibinnanthikattu@hotmail.com>
2022-06-27 19:11:59 +0200

    Feat:Function Components - 03

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <>
       {/* <Welcome  />    */}
-      <Sum numbers={numbers}/>
+      <Sum />
             </>
   );
 }
--- src/Sum.js
+++ src/Sum.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Sum(props) {
-    let sum = props.numbers.reduce((num1,num2)=>num1+num2)
+export default function Sum({props=20}) {
+    // let sum = props.numbers.reduce((num1,num2)=>num1+num2)
   return (
       <div>
-          <h1>Sum of the given values is : {sum}</h1>
+          <h1>Sum of the given values is : {props}</h1>
 
     </div>
   )
