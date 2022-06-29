0c7b1e9e04628871dbddfbfd11dceae46c8c3f96
Bibin Nandikattu Jose <bibinnanthikattu@hotmail.com>
2022-06-29 10:05:58 +0200

    Feat:useContext

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,30 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react'
 import { LanguageContext } from './LanguageContext'
 
-export default class DisplayLanguage extends Component {
-    render() {
-        const languages = {
-            en: {
-        CURRENT_LANGUAGE: "ENGLISH"
-            },
-            it: {
-        CURRENT_LANGUAGE: "ITALIAN"
-            },
-            mal: {
-                CURRENT_LANGUAGE: "MALAYALAM"
-            }
-}
+export default function DisplayLanguage() {
+    const language = useContext(LanguageContext)
+
+    const languages = {
+        en: {
+            CURRENT_LANGUAGE: "ENGLISH"
+        },
+        it: {
+            CURRENT_LANGUAGE: "ITALIAN"
+        },
+        mal: {
+            CURRENT_LANGUAGE: "MALAYALAM"
+        }
+    }
 
     return (
         <div>
-            <LanguageContext.Consumer>
-                {(language) => {
-                    return (
                         <h1>Current Language is : {languages[language].CURRENT_LANGUAGE}</h1>
-                    )
-                }}
-            </LanguageContext.Consumer>
       </div>
     )
   }
-}
+
